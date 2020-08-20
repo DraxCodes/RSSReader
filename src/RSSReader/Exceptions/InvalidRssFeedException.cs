1ba1334965124e6d7e98e0e59cee143e2e3d480b
Joel Parkinson <joelparkinson@live.co.uk>
2020-08-20 19:54:17 +0100

    Simplify Exceptions
    Remove unused constructors

--- src/RSSReader/Exceptions/InvalidRssFeedException.cs
+++ src/RSSReader/Exceptions/InvalidRssFeedException.cs
@@ -6,20 +6,9 @@ namespace RSSReader.Exceptions
     [Serializable]
     public class InvalidRssFeedException : Exception
     {
-        public InvalidRssFeedException()
-        {
-        }
 
-        public InvalidRssFeedException(string message) : base(message)
-        {
-        }
+        public InvalidRssFeedException(string message) : base(message) { }
 
-        public InvalidRssFeedException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
-
-        protected InvalidRssFeedException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-        }
+        protected InvalidRssFeedException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
--- src/RSSReader/Exceptions/InvalidUrlException.cs
+++ src/RSSReader/Exceptions/InvalidUrlException.cs
@@ -6,20 +6,8 @@ namespace RSSReader.Exceptions
     [Serializable]
     public class InvalidUrlException : Exception
     {
-        public InvalidUrlException()
-        {
-        }
+        public InvalidUrlException(string message) : base(message) { }
 
-        public InvalidUrlException(string message) : base(message)
-        {
-        }
-
-        public InvalidUrlException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
-
-        protected InvalidUrlException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-        }
+        protected InvalidUrlException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
